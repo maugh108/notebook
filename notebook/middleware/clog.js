29980af8bd881f1a57f6ebd4ed8d9c1201f92268
maugh108 <maugh108@outlook.com>
2022-11-27 20:11:14 -0600

    adecuaciones

--- notebook/middleware/clog.js
+++ notebook/middleware/clog.js
@@ -1,17 +1,16 @@
 // Middleware personalizado que cierra la sesión del tipo y la ruta de cada solicitud en el servidor
 const clog = (req, res, next) => {
-  const fgCyan = '\x1b[36m';
-  switch (req.method) {
+   switch (req.method) {
     case 'GET': {
-      console.info(`ðŸ“— ${fgCyan}${req.method} request to ${req.path}`);
+      console.info(`${req.method} request to ${req.path}`);
       break;
     }
     case 'POST': {
-      console.info(`ðŸ“˜ ${fgCyan}${req.method} request to ${req.path}`);
+      console.info(`${req.method} request to ${req.path}`);
       break;
     }
     default:
-      console.log(`ðŸ“™${fgCyan}${req.method} request to ${req.path}`);
+      console.log(`${req.method} request to ${req.path}`);
   }
 
   next();
--- notebook/server.js
+++ notebook/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { clog } = require('./middleware/clog');
 const api = require('./routes/index.js');
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -19,7 +19,7 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get('/', (req, res) =>
+app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
